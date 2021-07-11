22e0308ed70cd673c540687ab18dafa0724c53a5
Edgar Bahilo Rodríguez <edgar.bahilo_rodriguez@siemens.com>
2021-07-12 00:54:08 +0200

    adding log config and logger utils, starting data processing class

---
+++ code/processor.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from logger import Logger
+
+class DataProcessor:
+
+    def summary_file(self, df_in):
+        return
+
+    def summary_output(self, df_in):
+        return
+
+    def conversor(self, columns_in):
+        return
