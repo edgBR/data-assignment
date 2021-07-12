ecb30900ae7cb61ffeda209650197f918be13a38
Edgar Bahilo Rodríguez <edgar.bahilo_rodriguez@siemens.com>
2021-07-12 12:52:37 +0200

    starting processing functions

--- code/main.py
+++ code/main.py
@@ -1,1 +1,26 @@
+import os
+import time
+import datetime
+from logger import Logger
+from processor import *
+import numpy as np
+
+TEMP_FOLDER = os.getenv('TEMP_FOLDER', '/tmp/')
+logger = Logger.getLogger(__name__)
+
+def run_process(mode):
+    try:
+        input_df = pd.read_csv(f'{args.input_folder}/{args.file_name}')
+    except Exception as e:
+        raise e
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_folder', type=str, default=os.getenv('INPUT_FOLDER', '../data/input'))
+    parser.add_argument('--output_folder', type=str, default=os.getenv('INPUT_FOLDER', '../data/output'))
+    parser.add_argument('--file_name', type=str, default=os.getenv('FILE_NAME', 'btc.csv'))
+    parser.add_argument('--mode', type=str, default=os.getenv('MODE', 'all'))
+    args, _ = parser.parse_known_args()
+    run_process(args.mode)
+
 
--- code/processor.py
+++ code/processor.py
@@ -1,13 +1,64 @@
 import pandas as pd
 from logger import Logger
 
-class DataProcessor:
 
-    def summary_file(self, df_in):
-        return
 
-    def summary_output(self, df_in):
-        return
+def lag_processor(df_in, target_columns, lags=365, drop_nans=True) -> pd.DataFrame:
+    """
+    This function creates the lag values (previous values according to the index of the pd.Series)
+    for the columns mentioned in the target_columns and the steps specified in the lags argument
 
-    def conversor(self, columns_in):
-        return
+    Parameters
+    ----------
+    df_in : pd.DataFrame
+        Input dataframe
+    target_columns : list
+        A list containing the target columns that will be lagged
+    lags: int
+        An integer containing the lookback for calculating the previous data
+    drop_nans: bool
+        A boolean specifying if we drop the nans of the dataset or not
+
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe containing the lagged values of the target columns
+    """
+    try:
+        df_out = True
+        logger.info(f'Successfully lagged {target_columns}')
+    except Exception as e:
+        raise e
+
+    return df_out
+
+def summary_output(df_in):
+    return
+
+def conversor(df_in, columns_in, columns_out, conversion_ratio) -> pd.DataFrame:
+    """
+    This function converts the value of the columns within the columns_in list according to
+    a conversion ratio between EUROs and USD
+
+    Parameters
+    ----------
+    df_in : pd.DataFrame
+        Input dataframe
+    columns_in : list
+        A list containing the columns that will be converted to EURO
+    columns_out : list
+        A list containing the name of the output columns after the conversion
+    conversion_ratio : int
+        An integer with the value of the conversion ratio
+
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe containing the converted columns in EURO
+    """
+    try:
+        df_in[columns_out] = df_in[columns_in] * conversion_ratio
+        logger.info(f'Successfully converted {columns_in} to EURO')
+    except Exception as e:
+        raise e
+    return df_in
